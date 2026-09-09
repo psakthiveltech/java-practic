ac556fd4b91355b4c0721b3b91926f70b329bc99
sakthivel p <psakthivel.tech@gmail.com>
2026-09-09 09:32:32 +0530

    Delete Forloop.java

--- Forloop.java
+++
@@ -1,9 +0,0 @@
-package loops.forloop;
-
-public class Forloop {
-    public static void main(String[] args) {
-        for(int i=1; i<=10;i+=2){
-            System.out.println("the value of i is : "+i);
-        }
-    }
-}
